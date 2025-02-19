158dbeadf419e434c15ff9529966f194653fe802
Rsfrancis <arturfdamas2@gmail.com>
2025-02-19 00:08:27 +0000

    arquivo

---
+++ Exemplo1/Alguma.java
@@ -0,0 +1,13 @@
+public class Alguma {
+public static void main(String[] args) {
+   // System.out.println("Olá \nTurma!\\");
+    System.out.println("O dobro de 10.7 é " + dobro(10.7));
+}
+    
+    public static int dobro(int num) {
+        return 2 * num;
+    } 
+
+    public static double dobro(double num) {
+        return 2 * num;
+}
